4fc4b69fa1f8bc962845000512ec10ed0cf5ae0a
Sufiyan Budye <sufiyanbudye7@gmail.com>
2023-12-03 22:02:13 +0000

    Update 3 files

--- test.cpp
+++ test.cpp
@@ -1,2 +1,25 @@
 
-#include <stdio>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i=0;
+    cout << "Hello cpp";
+
+    int arr[3]={1};
+    cout << arr[10]; 
+
+    for(int i=0; i<3; i++){
+        cout << arr[i] << endl;
+    }
+    for(int x: arr){
+        cout << x << endl;
+    }
+    cout << sizeof(arr);
+
+    int n;
+    int arr2[n] = {1};
+
+    return 0;
+}
